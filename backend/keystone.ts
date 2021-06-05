a208c9dae5345c42872c096af748a981a6e23c13
salisu <salisshuaibu11@gmail.com>
2021-06-05 13:02:02 +0000

    Added User, Product, ProductImage schemas

--- backend/keystone.ts
+++ backend/keystone.ts
@@ -1,31 +1,66 @@
 import "dotenv/config";
 import { config, createSchema } from "@keystone-next/keystone/schema";
+import {
+  withItemData,
+  statelessSessions,
+} from "@keystone-next/keystone/session";
+import { createAuth } from "@keystone-next/auth";
+
+import { User } from "./schemas/User";
+import { Product } from "./schemas/Product";
+import { ProductImage } from "./schemas/ProductImage";
+
+import { insertSeedData } from "./seed-data/index";
 
 const databaseUrl =
   process.env.DATABASE_URL || "mongodb://localhost/keystone-sick-fits-tutorial";
 
-const sessionStorage = {
+const sessionConfig = {
   maxAge: 60 * 60 * 24 * 360, // show long a user stay signed in
-  secrec: process.env.COOKIE_SECRET,
+  secret: process.env.COOKIE_SECRET,
 };
 
-export default config({
-  server: {
-    cors: {
-      origin: [process.env.FRONTEND_URL],
-      credentials: true,
-    },
+const { withAuth } = createAuth({
+  listKey: "User",
+  identityField: "email",
+  secretField: "password",
+  initFirstItem: {
+    fields: ["name", "email", "password"],
+    //TODO: Add in initial roles here
   },
-  db: {
-    adapter: "mongoose",
-    url: databaseUrl,
-  },
-  lists: createSchema({
-    // Schema items will go here
-  }),
-  ui: {
-    // TODO: change this for roles
-    isAccessAllowed: () => true,
-  },
-  // TODO: Add session values here
 });
+
+export default withAuth(
+  config({
+    server: {
+      cors: {
+        origin: [process.env.FRONTEND_URL],
+        credentials: true,
+      },
+    },
+    db: {
+      adapter: "mongoose",
+      url: databaseUrl,
+      async onConnect(keystone) {
+        if (process.argv.includes("--seed-data")) {
+          await insertSeedData(keystone);
+        }
+      },
+    },
+    lists: createSchema({
+      // Schema items will go here
+      User,
+      Product,
+      ProductImage,
+    }),
+    ui: {
+      // show the UI only for people who pass the test
+      isAccessAllowed: ({ session }) => {
+        return session?.data;
+      },
+    },
+    session: withItemData(statelessSessions(sessionConfig), {
+      User: `id name email`,
+    }),
+  })
+);
